406924186734417747857f3f9501c3ff66dd2ba3
Franco <frcosati@hotmail.com>
2026-08-12 17:31:45 -0300

    improve performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Urbanist",
+    cssVariable: "--font-urbanist"
+  },
+  {
+   provider: fontProviders.fontsource(),
+   name: "Open Sans",
+   cssVariable: "--font-open-sans" 
+  }],
+  devToolbar: {
+    enabled: false
+  },
   vite: {
     plugins: [tailwindcss()]
   }
