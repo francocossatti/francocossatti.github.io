672480907d3471aa53951bae298b14a4fc8e07c1
Franco <frcosati@hotmail.com>
2026-09-25 02:01:51 -0300

    fix bug

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,11 +7,9 @@ export default function Contact() {
                 data-cal-namespace="30min"
                 data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":"true"}'
                 class='w-[clamp(5rem,5vw,10rem)] lg:w-24 flex items-center justify-center rounded-full bg-[#21201F] drop-shadow-lg aspect-square cursor-pointer hover:scale-105 transition-transform duration-200'
-            >
-                <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            ><svg class="pointer-events-none" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M30 8L40 18L30 28M40 18H16C13.8783 18 11.8434 18.8429 10.3431 20.3431C8.84285 21.8434 8 23.8783 8 26V40" stroke="#D3CFCA" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-            </button>
+</svg></button>
             
     
 }
